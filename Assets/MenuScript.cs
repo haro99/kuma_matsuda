ea7773389e82365760a6b5bb817953a30102ccb2
ko- matsu <tyuunichi77@gmail.com>
2021-10-18 21:04:22 +0900

    コミット_211018

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// すごろくステージでのメニュー
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+
+    public void TimeChange(float number)
+    {
+        Time.timeScale = number;
+    }
+
+    public void Rstart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SugorokuScene");
+    }
+    public void BackHome()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("HomeScene");
+    }
+}
